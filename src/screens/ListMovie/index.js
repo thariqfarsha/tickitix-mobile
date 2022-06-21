e5b6d52b21b5d34a50e6c1c3571d838bc04d91ed
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-22 02:46:16 +0700

    Integrate list movie screen

--- src/screens/ListMovie/index.js
+++ src/screens/ListMovie/index.js
@@ -1,6 +1,13 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {ScrollView, Text, TextInput, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import gs from '../../styles/globalStyles';
 import v from '../../styles/styleVariables';
 import SelectDropdown from 'react-native-select-dropdown';
@@ -8,22 +15,93 @@ import Icon from 'react-native-vector-icons/Feather';
 import MonthFilter from '../../components/MonthFilter';
 import MovieCard from '../../components/MovieCard';
 import Footer from '../../components/Footer';
+import axios from '../../utils/axios';
 
 export default function ListMovie(props) {
   const sortOptions = ['A-Z', 'Z-A'];
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const limit = 4;
+  const [sort, setSort] = useState('name asc');
+  const [search, setSearch] = useState('');
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [refresh, setRefresh] = useState(false);
+  const [loadMore, setLoadMore] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [last, setLast] = useState(false);
 
-  return (
-    <ScrollView
-      style={{backgroundColor: v.color.backgroundLight}}
-      showsVerticalScrollIndicator={false}>
-      <View style={gs.container}>
-        <Text style={gs.h2}>List Movies</Text>
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  useEffect(() => {
+    getMovies();
+  }, [page, month]);
+
+  const sortMonth = data => {
+    setMonth(data);
+    setPage(1);
+    console.log(data);
+  };
+
+  const getMovies = async () => {
+    try {
+      console.log('getmovie');
+      setRefresh(false);
+      setLoading(false);
+      setLoadMore(false);
+      if (page <= totalPage || totalPage === 0) {
+        const result = await axios.get(
+          `movie?page=${page}&limit=${limit}&searchName=${search}&sort=${sort}&searchRelease=${month}`,
+        );
+        if (page === 1) {
+          setMovies(result.data.data);
+        } else {
+          setMovies([...movies, ...result.data.data]);
+        }
+        setTotalPage(result.data.pagination.totalPage);
+      } else {
+        setLast(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRefresh = () => {
+    console.log('REFRESH SCREEN');
+    setPage(1);
+    setLast(false);
+    if (page !== 1) {
+      setRefresh(true);
+    } else {
+      getMovies();
+    }
+  };
+
+  const handleLoadMore = () => {
+    console.log('LOAD MORE DATA');
+    if (!loadMore) {
+      const newPage = page + 1;
+      setLoadMore(true);
+      if (newPage <= totalPage + 1) {
+        setLoading(true);
+        setPage(newPage);
+      } else {
+        setLoading(false);
+      }
+    }
+  };
+
+  const ListHeader = () => {
+    return (
+      <>
         <View style={{flexDirection: 'row', marginBottom: 16}}>
           <SelectDropdown
             data={sortOptions}
-            onSelect={(selectedItem, index) => {
-              console.log(selectedItem, index);
+            onSelect={selectedItem => {
+              setSort(selectedItem === 'Z-A' ? 'name desc' : 'name asc');
             }}
             buttonTextAfterSelection={selectedItem => selectedItem}
             rowTextForSelection={item => item}
@@ -54,29 +132,39 @@ export default function ListMovie(props) {
           />
         </View>
 
-        <MonthFilter month={month} setMonth={setMonth} />
-
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-          }}>
-          <View style={{marginBottom: 16, marginEnd: 16}}>
-            <MovieCard {...props} />
+        <MonthFilter month={month} setMonth={sortMonth} />
+      </>
+    );
+  };
+  console.log('month: ', month);
+  return (
+    <View style={{...gs.container, flex: 1}}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={movies}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <View style={{marginBottom: 20, marginHorizontal: 8}}>
+            <MovieCard {...props} data={item} />
           </View>
-          <View style={{marginBottom: 16, marginEnd: 0}}>
-            <MovieCard {...props} />
-          </View>
-          <View style={{marginBottom: 16, marginEnd: 16}}>
-            <MovieCard {...props} />
-          </View>
-          <View style={{marginBottom: 16, marginEnd: 0}}>
-            <MovieCard {...props} />
-          </View>
-        </View>
-      </View>
-      <Footer />
-    </ScrollView>
+        )}
+        onRefresh={handleRefresh}
+        refreshing={refresh}
+        onEndReached={handleLoadMore}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={() =>
+          last ? (
+            <View>
+              <Text style={{...gs.h5, textAlign: 'center'}}>.</Text>
+              {/* <Footer /> */}
+            </View>
+          ) : loading ? (
+            <ActivityIndicator size="large" color={v.color.primary} />
+          ) : null
+        }
+      />
+    </View>
   );
 }
