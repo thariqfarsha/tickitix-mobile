374392306ea142c338dba16d358c87b90691bc75
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-22 02:48:20 +0700

    Integrate payment screen

--- src/screens/Payment/index.js
+++ src/screens/Payment/index.js
@@ -1,17 +1,55 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  ActivityIndicator,
   ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import Footer from '../../components/Footer';
+import {createBooking} from '../../stores/actions/booking';
 import gs from '../../styles/globalStyles';
 import v from '../../styles/styleVariables';
+import axios from '../../utils/axios';
 
-export default function Payment() {
+export default function Payment(props) {
+  const dispatch = useDispatch();
+
+  const dataBooking = useSelector(state => state.booking.data);
+  const user = useSelector(state => state.user.data);
+
+  const {isLoading} = useSelector(state => state.booking);
+
+  const {
+    scheduleId,
+    dateBooking,
+    timeBooking,
+    paymentMethod,
+    totalPayment,
+    seats,
+  } = dataBooking;
+
+  const formBooking = {
+    scheduleId,
+    dateBooking,
+    timeBooking,
+    paymentMethod,
+    totalPayment,
+    seats,
+  };
+
+  const handlePayment = async () => {
+    try {
+      await dispatch(createBooking(formBooking));
+      props.navigation.navigate('Midtrans');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(formBooking);
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View
@@ -24,9 +62,18 @@ export default function Payment() {
           elevation: 10,
           marginBottom: 8,
         }}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Text style={{...gs.h3, fontWeight: '300'}}>Total Payment</Text>
-          <Text style={{...gs.h3}}>Rp 150.000</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={{...gs.h3, fontWeight: '300', marginBottom: 0}}>
+            Total Payment
+          </Text>
+          <Text style={{...gs.h2, marginBottom: 0}}>
+            {dataBooking.totalPayment}
+          </Text>
         </View>
       </View>
       <View style={{...gs.container, backgroundColor: 'transparent'}}>
@@ -40,19 +87,36 @@ export default function Payment() {
           }}>
           <View style={gs.inputGroup}>
             <Text style={gs.label}>Full Name</Text>
-            <TextInput style={gs.textInput} value="John Tyler" />
+            <TextInput
+              style={gs.textInput}
+              value={`${user.firstName} ${user.lastName}`}
+              editable={false}
+            />
           </View>
           <View style={gs.inputGroup}>
             <Text style={gs.label}>Email</Text>
-            <TextInput style={gs.textInput} value="johntyler@gmail.com" />
+            <TextInput
+              style={gs.textInput}
+              value={user.email}
+              editable={false}
+            />
           </View>
           <View style={{...gs.inputGroup, marginBottom: 4}}>
             <Text style={gs.label}>Phone Number</Text>
-            <TextInput style={gs.textInput} value="081234567890" />
+            <TextInput
+              style={gs.textInput}
+              value={user.noTelp}
+              editable={false}
+            />
           </View>
         </View>
-        <TouchableOpacity style={gs.btnPrimary}>
-          <Text style={gs.btnPrimaryText}>Pay your order</Text>
+        <TouchableOpacity
+          style={gs.btnPrimary}
+          activeOpacity={0.8}
+          onPress={handlePayment}>
+          <Text style={gs.btnPrimaryText}>
+            {isLoading ? <ActivityIndicator size="small" /> : 'Pay your order'}
+          </Text>
         </TouchableOpacity>
       </View>
       <Footer />
