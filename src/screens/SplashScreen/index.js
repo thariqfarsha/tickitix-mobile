48946c371a890488b50ba260c7f49f5b942efded
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-22 02:45:27 +0700

    Fix condition on splash screen

--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,27 +1,37 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useEffect} from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, StatusBar} from 'react-native';
+import v from '../../styles/styleVariables';
 
 function SplashScreen(props) {
   useEffect(() => {
-    setTimeout(() => {
-      const token = checkToken();
-      if (token) {
-        props.navigation.navigate('AppScreen');
-      } else {
-        props.navigation.navigate('AuthScreen');
-      }
-    }, 2000);
-  });
+    if (props.navigation?.navigate) {
+      checkToken();
+    }
+  }, [props.navigation]);
 
   const checkToken = async () => {
-    await AsyncStorage.getItem('token');
+    try {
+      const token = await AsyncStorage.getItem('token');
+      setTimeout(() => {
+        if (token) {
+          props.navigation.navigate('AppScreen');
+        } else {
+          props.navigation.navigate('AuthScreen');
+        }
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <View style={s.container}>
-      <Image source={require('../../assets/img/logo/logo-white.png')} />
-    </View>
+    <>
+      <StatusBar backgroundColor={v.color.primary} barStyle="light-content" />
+      <View style={s.container}>
+        <Image source={require('../../assets/img/logo/logo-white.png')} />
+      </View>
+    </>
   );
 }
 
