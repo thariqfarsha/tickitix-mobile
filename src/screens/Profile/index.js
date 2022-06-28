2db41b957ad72747786499611654c8e12174c402
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-29 01:56:21 +0700

    Integrate update and delete profile image

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -11,16 +11,24 @@ import {
   TextInput,
   ToastAndroid,
   ActivityIndicator,
+  Modal,
+  Pressable,
 } from 'react-native';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import Icon from 'react-native-vector-icons/Feather';
 import {useDispatch, useSelector} from 'react-redux';
 import BookingCard from '../../components/BookingCard';
 import HLine from '../../components/HLine';
-import {getUserById, updateProfile, updatePwd} from '../../stores/actions/user';
+import {
+  getUserById,
+  logout,
+  updateProfile,
+  updatePwd,
+} from '../../stores/actions/user';
 import gs from '../../styles/globalStyles';
 import v from '../../styles/styleVariables';
 import axios from '../../utils/axios';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 // const renderScene = SceneMap({
 //   profile: ProfileRoute,
@@ -72,18 +80,121 @@ export default function Profile(props) {
       newPassword: '',
       confirmPassword: '',
     });
+    const [modalVisible, setModalVisible] = useState(false);
+    const [confirmModalVisible, setConfirmModalVisible] = useState(false);
+    const [isUpload, setIsUpload] = useState(false);
+
+    const handleLaunchCamera = async () => {
+      try {
+        setIsUpload(true);
+        setModalVisible(!modalVisible);
+        const photo = await launchCamera({
+          mediaType: 'photo',
+          maxWidth: 100,
+          quality: 0.2,
+          cameraType: 'front',
+        });
+        const formData = new FormData();
+        formData.append('image', {
+          name: photo.assets[0].fileName,
+          type: photo.assets[0].type,
+          uri: photo.assets[0].uri,
+        });
+        const result = await axios.patch(`user/image/${user.id}`, formData);
+        await dispatch(getUserById(user.id));
+        setIsUpload(false);
+        setIsUpload(false);
+        ToastAndroid.showWithGravity(
+          `${result.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+      } catch (error) {
+        console.log(error.response.data);
+        setIsUpload(false);
+        ToastAndroid.showWithGravity(
+          `${error.response.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+      }
+    };
+
+    const handleLaunchImageLibrary = async () => {
+      try {
+        setModalVisible(!modalVisible);
+        const photo = await launchImageLibrary({
+          mediaType: 'photo',
+          maxWidth: 100,
+        });
+        setIsUpload(true);
+        const formData = new FormData();
+        formData.append('image', {
+          name: photo.assets[0].fileName,
+          type: photo.assets[0].type,
+          uri: photo.assets[0].uri,
+        });
+        const result = await axios.patch(`user/image/${user.id}`, formData);
+        await dispatch(getUserById(user.id));
+        setIsUpload(false);
+        ToastAndroid.showWithGravity(
+          `${result.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+      } catch (error) {
+        console.log(error.response.data);
+        setIsUpload(false);
+        ToastAndroid.showWithGravity(
+          `${error.response.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+      }
+    };
+
+    const handleDeleteImage = () => {
+      setModalVisible(!modalVisible);
+      setConfirmModalVisible(true);
+    };
+
+    const handleConfirmDeleteImage = async () => {
+      try {
+        setIsUpload(true);
+        setConfirmModalVisible(false);
+        const result = await axios
+          .delete(`user/image/${user.id}`)
+          .then(setIsUpload(false));
+        ToastAndroid.showWithGravity(
+          `${result.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+        await dispatch(getUserById(user.id));
+      } catch (error) {
+        console.log(error);
+        setIsUpload(false);
+        ToastAndroid.showWithGravity(
+          `${error.response.data.msg}`,
+          ToastAndroid.SHORT,
+          ToastAndroid.BOTTOM,
+        );
+      }
+    };
 
     const handleLogout = async () => {
       try {
+        const refreshToken = await AsyncStorage.getItem('refreshToken');
+        await dispatch(logout(refreshToken));
+        await AsyncStorage.clear();
+        props.navigation.navigate('AuthScreen', {
+          screen: 'Login',
+        });
         ToastAndroid.showWithGravity(
           'Logged out',
           ToastAndroid.SHORT,
           ToastAndroid.BOTTOM,
         );
-        await AsyncStorage.clear();
-        props.navigation.navigate('AuthScreen', {
-          screen: 'Login',
-        });
       } catch (error) {
         console.log(error);
       }
@@ -131,6 +242,12 @@ export default function Profile(props) {
       }
     };
 
+    const profilePicture = {
+      uri: user.imagePath
+        ? user.imagePath
+        : `https://ui-avatars.com/api/?name=${user.firstName}+${user.lastName}&background=random&size=192`,
+    };
+
     return (
       <ScrollView
         showsVerticalScrollIndicator={false}
@@ -144,20 +261,240 @@ export default function Profile(props) {
               alignItems: 'center',
               marginBottom: 32,
             }}>
-            <Image
-              source={
-                user.imagePath
-                  ? {uri: user.imagePath}
-                  : require('../../assets/img/blankProfile.png')
-              }
-              style={{
-                height: 100,
-                width: 100,
-                resizeMode: 'cover',
-                borderRadius: 100,
-                marginBottom: 28,
-              }}
-            />
+            <TouchableOpacity
+              style={{position: 'relative'}}
+              activeOpacity={0.8}
+              onPress={() => setModalVisible(true)}>
+              <Image
+                source={profilePicture}
+                style={{
+                  height: 100,
+                  width: 100,
+                  resizeMode: 'cover',
+                  borderRadius: 100,
+                  marginBottom: 28,
+                }}
+              />
+              <View
+                style={{
+                  backgroundColor: v.color.white,
+                  elevation: 2,
+                  borderRadius: 100,
+                  width: 24,
+                  height: 24,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  position: 'absolute',
+                  right: 4,
+                  bottom: 32,
+                }}>
+                <Icon name="edit-2" color={v.color.body} size={12} />
+              </View>
+              {(isUpload || isLoading) && (
+                <ActivityIndicator
+                  size="large"
+                  color={v.color.primary}
+                  style={{position: 'absolute', top: 33, left: 33}}
+                />
+              )}
+            </TouchableOpacity>
+            {/* Modal Choose Photo*/}
+            <Modal
+              animationType="fade"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Pressable
+                style={{
+                  height: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                }}
+                onPress={() => setModalVisible(false)}>
+                <View
+                  style={{
+                    margin: 20,
+                    backgroundColor: 'white',
+                    borderRadius: 12,
+                    alignItems: 'flex-start',
+                    shadowColor: '#000',
+                    elevation: 5,
+                    paddingVertical: 16,
+                    width: '60%',
+                  }}>
+                  <Text
+                    style={{
+                      ...gs.h5,
+                      paddingVertical: 8,
+                      textAlign: 'center',
+                      width: '100%',
+                    }}>
+                    Choose picture
+                  </Text>
+                  <Pressable
+                    style={{
+                      paddingVertical: 12,
+                      paddingHorizontal: 24,
+                      borderRadius: 12,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      width: '100%',
+                    }}
+                    android_ripple={{color: v.color.line}}
+                    onPress={handleLaunchCamera}>
+                    <Icon name="camera" size={16} />
+                    <Text
+                      style={{
+                        ...gs.p,
+                        marginBottom: 0,
+                        marginStart: 8,
+                      }}>
+                      Take picture using camera
+                    </Text>
+                  </Pressable>
+                  <Pressable
+                    style={{
+                      paddingVertical: 12,
+                      paddingHorizontal: 24,
+                      borderRadius: 12,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      width: '100%',
+                    }}
+                    android_ripple={{color: v.color.line}}
+                    onPress={handleLaunchImageLibrary}>
+                    <Icon name="image" size={16} />
+                    <Text
+                      style={{
+                        ...gs.p,
+                        marginBottom: 0,
+                        marginStart: 8,
+                      }}>
+                      Pick from gallery
+                    </Text>
+                  </Pressable>
+                  <Pressable
+                    style={{
+                      paddingVertical: 12,
+                      paddingHorizontal: 24,
+                      borderRadius: 12,
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      width: '100%',
+                      opacity: user.imagePath ? 1 : 0.5,
+                    }}
+                    android_ripple={{color: v.color.line}}
+                    onPress={handleDeleteImage}
+                    disabled={user.imagePath ? false : true}>
+                    <Icon name="trash-2" size={16} color={v.color.error} />
+                    <Text
+                      style={{
+                        ...gs.p,
+                        marginBottom: 0,
+                        marginStart: 8,
+                        color: v.color.error,
+                      }}>
+                      Delete picture
+                    </Text>
+                  </Pressable>
+                </View>
+              </Pressable>
+            </Modal>
+            {/* Modal Confirm Deletion */}
+            <Modal
+              animationType="fade"
+              transparent={true}
+              visible={confirmModalVisible}
+              onRequestClose={() => {
+                setConfirmModalVisible(!confirmModalVisible);
+              }}>
+              <Pressable
+                style={{
+                  height: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                }}
+                onPress={() => setConfirmModalVisible(false)}>
+                <View
+                  style={{
+                    margin: 20,
+                    backgroundColor: 'white',
+                    borderRadius: 12,
+                    alignItems: 'flex-start',
+                    shadowColor: '#000',
+                    elevation: 5,
+                    padding: 16,
+                    width: '60%',
+                  }}>
+                  <Text
+                    style={{
+                      ...gs.h5,
+                      paddingVertical: 8,
+                      textAlign: 'center',
+                      width: '100%',
+                    }}>
+                    Confirm Deletion
+                  </Text>
+                  <Text
+                    style={{
+                      ...gs.p,
+                      paddingVertical: 8,
+                      textAlign: 'center',
+                      width: '100%',
+                    }}>
+                    Are you sure to delete your profile picture?
+                  </Text>
+                  <View style={{flexDirection: 'row', width: '100%'}}>
+                    <Pressable
+                      style={{
+                        paddingVertical: 12,
+                        paddingHorizontal: 24,
+                        borderRadius: 12,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+
+                        flex: 1,
+                      }}
+                      android_ripple={{color: v.color.line}}
+                      onPress={() => setConfirmModalVisible(false)}>
+                      <Text
+                        style={{
+                          ...gs.p,
+                          marginBottom: 0,
+                        }}>
+                        Cancel
+                      </Text>
+                    </Pressable>
+                    <Pressable
+                      style={{
+                        paddingVertical: 12,
+                        paddingHorizontal: 24,
+                        borderRadius: 12,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        flex: 1,
+                      }}
+                      android_ripple={{color: v.color.line}}
+                      onPress={handleConfirmDeleteImage}>
+                      <Text
+                        style={{
+                          ...gs.p,
+                          marginBottom: 0,
+                          color: v.color.error,
+                        }}>
+                        Delete
+                      </Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </Pressable>
+            </Modal>
             <Text
               style={{
                 ...gs.h2,
@@ -364,6 +701,10 @@ export default function Profile(props) {
 
     useEffect(() => {
       getBookingList();
+
+      return () => {
+        setBookingList({});
+      };
     }, []);
 
     const getBookingList = async () => {
