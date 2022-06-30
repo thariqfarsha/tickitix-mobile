3c6a8b4883f6a7b73200c95f10309a084f362e45
Thariq Farsha <mthariqfarsha@gmail.com>
2022-06-30 15:19:02 +0700

    Add notif screen playground

--- src/screens/Notification/index.js
+++ src/screens/Notification/index.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import Notification from '../../utils/notif';
 
-export default function Notification() {
+export default function NotificationScreen() {
+  const handleClickNotif = () => {
+    console.log('notif clicked');
+
+    // [without schedule]
+    // Notification.reminderProductNotification();
+
+    // [with schedule]
+    const setNotification = {
+      title: 'New Product',
+      message: 'Buy it now',
+      date: new Date(Date.now() + 5 * 1000),
+    };
+    console.log(setNotification);
+    Notification.scheduleProductNotification(setNotification);
+  };
   return (
     <View style={s.container}>
       <Text>Notif</Text>
+      <Button title="Reminder Product" onPress={handleClickNotif} />
+      <Text>-------------</Text>
+      <Button
+        title="get delivered notif"
+        onPress={() => Notification.getDeliveredNotifications()}
+      />
+      <Text>-------------</Text>
+      <Button
+        title="get scheduled notif"
+        onPress={() => Notification.getScheduledLocalNotifications()}
+      />
+      <Text>-------------</Text>
+      <Button
+        title="cancel all notif"
+        onPress={() => Notification.cancelAllLocalNotifications()}
+      />
     </View>
   );
 }
